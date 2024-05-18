f82c14764bfc65d16078ca45314b13a69d51ba77
Isaac <isaacmireland@gmail.com>
2024-05-18 12:33:36 +0100

    amending output

--- data-science/feature-creation.py
+++ data-science/feature-creation.py
@@ -3,6 +3,7 @@
 
 import snowflake.snowpark as snowpark
 from snowflake.snowpark.functions import col
+from sklearn.preprocessing import StandardScaler
 
 # Calculate technical indicators
 def calculate_technical_indicators(df):
@@ -34,6 +35,8 @@ def calculate_technical_indicators(df):
     df['Upper_Band'] = df['Middle_Band'] + (df['CLOSE'].rolling(window=20).std() * 2)
     df['Lower_Band'] = df['Middle_Band'] - (df['CLOSE'].rolling(window=20).std() * 2)
     
+    # RL models features
+    # 
     return df
 
 def main(session: snowpark.Session): 
@@ -44,8 +47,22 @@ def main(session: snowpark.Session):
     df = snowframe.to_pandas()
     df = calculate_technical_indicators(df)
 
-    print(df.head())
+    features_to_scale = ['SMA_5', 'SMA_10', 'EMA_5', 'EMA_10', 'RSI', 'MACD', 'Signal_Line', 'Middle_Band', 'Upper_Band', 'Lower_Band']
+    scaler = StandardScaler()
+    df[features_to_scale] = scaler.fit_transform(df[features_to_scale])
+
+    features = df[['open', 'high', 'low', 'close', 'volume', 'trades', 'SMA_5', 'SMA_10', 'EMA_5', 'EMA_10', 'RSI', 'MACD', 'Signal_Line', 'Middle_Band', 'Upper_Band', 'Lower_Band']]
     snowframe = session.create_dataframe(df)
+    # features_array = features.to_numpy()
     
     # Return value will appear in the Results tab.
     return snowframe
+
+
+# # Drop rows with NaN values (caused by rolling calculations)
+# df.dropna(inplace=True)
+
+# # Normalize the data
+
+# # Convert the features to a numpy array for the RL model
+# features_array = features.to_numpy()
