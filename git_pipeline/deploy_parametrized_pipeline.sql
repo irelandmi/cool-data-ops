232506aa87faad079880d2994bb48c7f278f9c3c
irelandmi <isaacmireland@gmail.com>
2024-08-29 20:02:16 +0100

    Update deploy_parametrized_pipeline.sql

--- git_pipeline/deploy_parametrized_pipeline.sql
+++ git_pipeline/deploy_parametrized_pipeline.sql
@@ -1,1 +1,1 @@
-execute immediate from 'git_pipeline/steps/01_setup_raws.sql' using (environment => '{{environment}}');
+execute immediate from 'steps/01_setup_raws.sql' using (environment => '{{environment}}');
