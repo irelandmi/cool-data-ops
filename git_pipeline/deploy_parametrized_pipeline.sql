e8e15ded06834cdd29b4ad7a9dfc17a37a6e5561
Isaac Ireland <isaacmireland@gmail.com>
2025-01-02 18:01:38 +0000

    adding new tables

--- git_pipeline/deploy_parametrized_pipeline.sql
+++ git_pipeline/deploy_parametrized_pipeline.sql
@@ -1,1 +1,2 @@
+-- execute immediate from 'steps/01_setup_stages.sql' using (environment => '{{environment}}');
 execute immediate from 'steps/01_setup_raws.sql' using (environment => '{{environment}}');
