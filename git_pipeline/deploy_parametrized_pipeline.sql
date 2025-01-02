9b4f7c6667e2e629f27760e45994740263627cf7
Isaac Ireland <isaacmireland@gmail.com>
2025-01-02 20:12:21 +0000

    removing comment and code

--- git_pipeline/deploy_parametrized_pipeline.sql
+++ git_pipeline/deploy_parametrized_pipeline.sql
@@ -1,2 +1,1 @@
--- execute immediate from 'steps/01_setup_stages.sql' using (environment => '{{environment}}');
 execute immediate from 'steps/01_setup_raws.sql' using (environment => '{{environment}}');
